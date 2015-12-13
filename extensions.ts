6b1f4159b6edce086be0eb3ad7f62e7a7c0b0a8a
Max Koverdyaev <shadeglare@gmail.com>
2015-12-13 23:37:27 +0300

    old

---
+++ extensions.ts
@@ -0,0 +1,33 @@
+export function id<T>(x: T): T {
+    return x;
+}
+
+export function takeWhile<T>(xs: T[], callback: (x: T, i: number) => boolean): T[] {
+    let acc: T[] = [];
+    xs.some((x, i) => {
+        if (callback(x, i)) {
+            acc.push(x);
+            return false;
+        } else {
+            return true;
+        }
+    });
+    return acc;
+}
+
+export function countWhile<T>(xs: T[], callback: (x: T, i: number) => boolean): number {
+    let count = 0;
+    xs.some((x, i) => {
+        if (callback(x, i)) {
+            count++;
+            return false;
+        } else {
+            return true;
+        }
+    });
+    return count;
+}
+
+export function daysInMonth(year: number, month: number): number {
+    return new Date(year, month, 0).getDate();
+}
--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
+import * as _ from "./extensions";
+
 import * as Check from "./check";
 import * as Singleton from "./singleton";
 import * as Types from "./types";
 
 import Queue from "./queue";
 import Stack from "./stack";
+
 import * as Point2 from "./point2";
 
 export {
+    _,
     Check,
     Singleton,
     Types,
